ead201db5b4fb2d103689e5410f53fdcd7be327c
Shyamb268 <shyambandi532@gmail.com>
2025-07-30 14:57:34 +0530

    Update mem_interface.v

--- mem_interface.v
+++ mem_interface.v
@@ -2,18 +2,23 @@ module mem_interface (
     input wire clk,
     input wire we,
     input wire [18:0] addr,
-    inout wire [18:0] data,
+    inout wire [18:0] data;
     output reg [18:0] mem_out,
     input wire [18:0] mem_in
+    
 );
-    reg [18:0] memory [0:1023]; // 1K x 19-bit memory
 
-    always @(posedge clk) begin
-        if (we) begin
-            memory[addr] <= mem_in;
-        end
-        mem_out <= memory[addr];
+reg [18:0] memory [0:1023]; //1K x 19-bit memory
+
+always@(posedge clk) begin
+
+     if (we) begin
+        memory[addr] <=mem_in
+
     end
+    mem_out <=memory[addr]
+end
 
-    assign data = we ? 19'bz : mem_out;
-endmodule 
+assign data = we ? 19'bz: mem_out;
+
+endmodule
