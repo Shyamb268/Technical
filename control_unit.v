187b5754b542ac7c7abd2189fa916a7515761fb6
Shyamb268 <shyambandi532@gmail.com>
2025-07-30 14:56:21 +0530

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,4 +1,4 @@
-module control_unit (
+module control_unit(
     input wire [18:0] instr,
     output reg [4:0] opcode,
     output reg [3:0] rd,
@@ -6,13 +6,16 @@ module control_unit (
     output reg [3:0] rs2,
     output reg [9:0] imm,
     output reg [13:0] addr
-);
-    always @(*) begin
-        opcode = instr[18:14];
-        rd     = instr[13:10];
-        rs1    = instr[9:6];
-        rs2    = instr[5:2];
-        imm    = instr[9:0];
-        addr   = instr[13:0];
-    end
-endmodule 
+ );
+ 
+ always @ (*)  begin
+   opcode = instr[18:14];
+   rd = instr[13:10];
+   rs1 = instr[9:6];
+   rs2 = instr[5:2];
+   imm = instr[9:0];
+   addr = instr[13:0];
+
+end
+
+end module
